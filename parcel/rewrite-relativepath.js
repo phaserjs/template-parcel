30039a6d97f50becebddbdc4d8ba12b3e569cc1a
Francisco Pereira Alvarado <gammafp@gmail.com>
2024-01-29 15:01:49 +0100

    hearts fixed

--- parcel/rewrite-relativepath.js
+++ parcel/rewrite-relativepath.js
@@ -15,7 +15,7 @@ module.exports = new Reporter({
                 }
             }
             const line = "---------------------------------------------------------";
-            const msg = `💜💜💜 Tell us about your game! - games@phaser.io 💜💜💜`;
+            const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
             process.stdout.write(`${line}\n${msg}\n${line}\n`);
             process.stdout.write(`✨ Built ${bundles.length} bundles in ${event.buildTime}ms!\n`);
         }
